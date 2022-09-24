b2e21c73feff5863b5b18110e0327c7a1ef483bb
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-09-24 17:29:37 +0200

    changed in Movie.php

--- movie.php
+++ movie.php
@@ -17,6 +17,6 @@ class Movie {
     }
 
     public function getMovieInfo() {
-        echo '<div> <img src="' . $this->poster . '" alt="'. $this->title .' poster" /> </div><h2>Titolo: ' . $this->title . '</h2> <h3>Regista: '  . $this->director .  '</h3> <div>Anno: '  . $this->release_year .  '</div> <p>Trama: ' . $this->plot . '</p>';
+        echo '<div> <img src="' . $this->poster . '" alt="'. $this->title .' poster" /> </div> <h2>Titolo: ' . $this->title . '</h2> <h3>Regista: '  . $this->director .  '</h3> <div>Anno: '  . $this->release_year .  '</div> <p>Trama: ' . $this->plot . '</p>';
     }
 }
